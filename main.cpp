4fa7f5131b16fc91a9579029ea7dacbb7f53c38d
Hieu Quang <hquang@unm.edu>
2022-11-17 15:27:08 -0700

    Update: Cleaned main

--- main.cpp
+++ main.cpp
@@ -1,45 +1,21 @@
 #include <iostream>
-#include <stdlib.h>
 #include <random> 
-#include "mpi/mpi.h"
-
-double randomizer(double min, double max) {
-    double x = (double)rand() / RAND_MAX;
-    return (min + x * (max - min));
-}
-
-void find_min(long local_size, long limit) {
-    int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-    double lower_bound = 0;
-    double upper_bound = 10000;
-    double temp = 0.0;
-    double output = randomizer(lower_bound,upper_bound);
-
-    // Linear search to find the MIN value
-    for (long i = 0; i < local_size; i++) {
-        temp = randomizer(lower_bound,upper_bound);
-        if (temp < output) {
-            output = temp;
-        } 
-    }
-
-    std::cout << "Rank " << rank << " | Out:" << output << std::endl;
-}
+#include "linear_search.hpp"
 
 int main (int argc, char* argv[]) {
     MPI_Init(&argc, &argv);
-    srand(time(NULL));
 
+    float time;
     int rank, num_procs;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
 
-    long limit = 1 << 10;
+    long limit = 1 << 25;
     long local_size = limit / num_procs;
 
-    find_min(local_size, limit);
+    time = find_min_time(local_size);
+
+    std::cout << "Rank " << rank << " | Time: " << time << "s" << std::endl;
 
     MPI_Finalize();
     return 0;
