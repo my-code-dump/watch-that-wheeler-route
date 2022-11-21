238e10f815a3a19a9758958c55dbb75f02bf3a31
Hieu Quang <hquang@unm.edu>
2022-11-21 02:30:31 -0700

    Update: added iterations for main

--- main.cpp
+++ main.cpp
@@ -7,16 +7,23 @@ int main (int argc, char* argv[]) {
 
     float time;
     int rank, num_procs;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+	
+	if (rank == 0) {
+		std::cout << num_procs << " Procs" << std::endl;
+	}
 
-    long limit = 1 << 25;
-    long local_size = limit / num_procs;
+    for (int sz = 10; sz < 25; sz++) {
+		long limit = 1 << sz;
+		long local_size = limit / num_procs;
+		
+		time = find_min_time(local_size);
 
-    time = find_min_time(local_size);
-
-    std::cout << "Rank " << rank << " | Time: " << time << "s" << std::endl;
-
+		if (rank == 0) { 
+			std::cout << "2^" << sz << " | Time: " << time << "s" << std::endl;
+		}
+	}
     MPI_Finalize();
     return 0;
 }
